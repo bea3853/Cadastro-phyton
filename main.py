7519f6c6d74deaddc8db5d7c578fb9084de8d348
Bea Alves <blues5832@gmail.com>
2021-01-15 21:13:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+#Código em Phyton#
+
+name = 'BEATRIZ'
+print('Nome:', name)
+
+data = '26/11/2011'
+print('Data de nascimento:', data)
+
+sexo = "F"
+print ('sexo:',sexo)
+
+idade = 25
+print ('Idade:', idade)
+
+contato = 'Phone: (11)9090-90909'
+print (contato)
+
+cpf = 'CPF: 2323.2323.2323'
+print(cpf)
+
+rg = 'RG: 2323.2323.2323'
+print(rg)
+
+endereco = 'Endereço: Reinaldo Cesar, 320, SP'
+print(endereco)
+
+email = 'Email: b@bmail.com'
+print (email)
+
+telefone = 'Telefone: 12. 2222-2222'
+print (telefone)
+
+sexo = 'Sexo:F'
+print (sexo)
+
+pi = 1000.00
+print("Salario:", pi)
+
+vinculo = True
+print('Vinculo empregaticio:',vinculo)
+
+irf = 1000*0.15 
+print('O imposto é de R$:',irf)
